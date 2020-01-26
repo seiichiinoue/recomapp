c0a95f31afdf3a7b754fbd43e62e232311ddfebb
Seiichi Inoue <d35inou108@gmail.com>
2020-01-26 15:01:02 +0900

    small change

--- app.py
+++ app.py
@@ -14,8 +14,11 @@ def find_sim_docs(tar, model_path="./model/lda.model"):
         corpus = pickle.load(f)
     with open("./model/id2doc.pickle", "rb") as f:
         id2doc = pickle.load(f)
+    with open("./model/id2title.pickle", "rb") as f:
+        id2title = pickle.load(f)
     doc2id = {doc:id for id, doc in id2doc.items()}
-    # print(doc2id)
+    title2id = {title:id for id, title in id2title.items()}
+    # print(title2id)
     with open("./model/dictionary.pickle", "rb") as f:
         dictionary = pickle.load(f)
     model = gensim.models.ldamodel.LdaModel.load(model_path)
@@ -32,12 +35,13 @@ def find_sim_docs(tar, model_path="./model/lda.model"):
         topics[i] = topic_index
     # print(topics)
     # find similar documents from topic list
-    target_topic = topics[doc2id[target_document]]
-    sim_docs = []
+    target_topic = topics[title2id[target_document]]
+    sim_docs_name, sim_docs_title = [], []
     for doc_id, topic in enumerate(topics):
-        if topic == target_topic and doc_id != doc2id[target_document]:
-            sim_docs.append(id2doc[doc_id])
-    return sim_docs
+        if topic == target_topic and doc_id != title2id[target_document]:
+            sim_docs_name.append(id2doc[doc_id])
+            sim_docs_title.append(id2title[doc_id])
+    return sim_docs_name, sim_docs_title
 
 @app.route("/infer", methods=["POST","GET"])
 def infer():
@@ -47,18 +51,22 @@ def infer():
     }
     # ensure an feature was properly uploaded to our endpoint
     if flask.request.method == "POST":
-        if flask.request.get_json().get("filename"):
+        if flask.request.get_json().get("title"):
             # read feature from json
-            filename = flask.request.get_json().get("filename")
-            # classify the input feature
-            response["prediction"] = find_sim_docs(filename)
+            title = flask.request.get_json().get("title")
+            # predict similar documents of requested document
+            names, titles = find_sim_docs(title)
+            response["similar_doc_name"] = names
+            response["similar_doc_title"] = titles
             # indicate that the request was a success
             response["success"] = True
 
     elif flask.request.method == "GET":
-        if flask.request.args.get("filename"):
-            filename = flask.request.args.get("filename")
-            response["prediction"] = find_sim_docs(filename)
+        if flask.request.args.get("title"):
+            title = flask.request.args.get("title")
+            names, titles = find_sim_docs(title)
+            response["similar_doc_name"] = names
+            response["similar_doc_title"] = titles
             response["success"] = True
 
     # return the data dictionary as a JSON response
